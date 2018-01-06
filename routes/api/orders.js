41a150b94a36890c3194ae41e86534f5675be971
Duy Ngo <ntduy96@gmail.com>
2018-01-06 19:17:11 +0700

    parse basic request body on some routes

--- routes/api/orders.js
+++ routes/api/orders.js
@@ -8,8 +8,14 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
+  const order = {
+      productId: req.body.productId,
+      quantity: req.body.quantity
+  };
+
   res.status(201).json({
-    message: 'Order is created'
+    message: 'Order is created',
+    order: order
   });
 });
 
--- routes/api/products.js
+++ routes/api/products.js
@@ -8,8 +8,14 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
+  const product = {
+    name: req.body.name,
+    price: req.body.price
+  };
+
   res.status(201).json({
-    message: 'Handling POST request to /products'
+    message: 'Handling POST request to /products',
+    productCreated: product
   });
 });
 
