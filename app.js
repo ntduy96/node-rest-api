f1fe3e2fe8627d8e8cada473234a7a6e602d0cc0
Duy Ngo <ntduy96@gmail.com>
2018-01-06 18:37:05 +0700

    add morgan to display log in development environment

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const morgan = require('morgan');
 
 const products = require("./routes/api/products");
 const orders = require("./routes/api/orders");
+
+app.use(morgan('dev'));
 
 app
   .use("/products", products)
