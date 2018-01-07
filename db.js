56a9b5b3e9334d9d00d17cbef9ed8c5bb2f729df
Duy Ngo <ntduy96@gmail.com>
2018-01-08 01:22:50 +0700

    update connection uri due to mongodb atlas database version

--- db.js
+++ db.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 /**
  * Open a connection to the `test` database on MongoDB Atlas
  */
-mongoose.connect('mongodb+srv://myTester:'+ process.env.MONGO_ATLAS_PSW +'@node-rest-shop-wzzlz.mongodb.net/test');
+// Change connection uri due to MongoDB Atlas database running on version 3.4.10
+mongoose.connect('mongodb://myTester:' + process.env.MONGO_ATLAS_PSW + '@node-rest-shop-shard-00-00-wzzlz.mongodb.net:27017,node-rest-shop-shard-00-01-wzzlz.mongodb.net:27017,node-rest-shop-shard-00-02-wzzlz.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin');
 
 const db = mongoose.connection;
 
